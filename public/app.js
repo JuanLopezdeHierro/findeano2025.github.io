af0e4a89ea61a19cc21cda22b0ef4a9dcd83b079
unknown <juanlopezdehierro@gmail.com>
2025-12-18 13:23:59 +0000

    Add: Navigation to tabla.html and all pages

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // ===== GOOGLE SHEETS INTEGRATION =====
 const SHEET_URL = 'https://script.google.com/macros/s/AKfycbwHv2U_nX7lxjKZnPbce0GbTYjkdjnWPe5eN9mSIwxCC1oapWhb33L6JQJ8WZRPZOo9rQ/exec';
 
-const USERS = ['Juan', 'Quintana', 'Alejandro', 'Arbelo', 'Cantero', 'Germán', 'David', 'Jorge', 'Jose'];
+const USERS = ['Juan', 'Quintana', 'Alejandro', 'Arbelo', 'Cantero', 'German', 'David', 'Jorge', 'Jose'];
 
 const CATEGORIES = [
     { id: 'Alcohol', name: 'Alcohol', icon: 'wine_bar', limit: 112.5 },
